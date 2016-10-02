4b4193fe77b8037b2f06bf894e85fb09632da33c
Joe McManus <joe.mcmanus@colorado.edu>
2016-10-01 23:26:55 -0600

    Changes...

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # a01
 A collection of programs for the Olympus Air A01 OPC 
+
+The Olympus Air A01 is a unique camera that is designed around an open platform. 
+This repo will hold a collection of scripts to run on the device. 
+More features will be added as I play with this more. 
+
+# a01.py
+This program will enable picture taking via command line on python.
+
+    usage: a01.py [-h] [--pid] [--interval INTERVAL] [--count COUNT] [--debug]
+
+    Olympus Air A01 Control Program. (C) Joe McManus 2016
+
+    optional arguments:
+      -h, --help           show this help message and exit
+      --pid                Create a pid file in /var/run/a01.pid
+      --interval INTERVAL  Take pictures at X interval in seconds.
+      --count COUNT        Take X pictures
+      --debug              Enable debug messages
+
+To take one photo just run ./a01.py.
+
+     sazed:a01 joe$ ./a01.py 
+     Taking 1 photo.
+
+To take 5 photos at 3 seconds each provide the --interval and --count options. 
+
+    sazed:a01 joe$ ./a01.py  --interval 5 --count 3
+    Setting Intervalometer to take 5 pictures.
+    Taking a total of 3 pictures.
+    Taking picture 1 of 3 
+    Sleeping for 5 seconds.
+    Taking picture 2 of 3 
+    Sleeping for 5 seconds.
+    Taking picture 3 of 3 
+
+To enable debug messages use the --debug option. 
+
+    http://192.168.0.10/get_connectmode.cgi
+    <?xml version="1.0"?>
+    <connectmode>OPC</connectmode>
+    http://192.168.0.10/switch_cameramode.cgi?mode=rec
+    <?xml version="1.0"?><result>OK</result>
+    .....
+
--- a01.py
+++ a01.py
@@ -18,6 +18,35 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import requests
+import time
+import argparse
+
+parser = argparse.ArgumentParser(description='Olympus Air A01 Control Program.\n (C) Joe McManus 2016')
+parser.add_argument('--pid', help="Create a pid file in /var/run/a01.pid",  action="store_true")
+parser.add_argument('--interval', help="Take pictures at X interval in seconds.", type=int, action="store")
+parser.add_argument('--count', help="Take X pictures", type=int, action="store")
+parser.add_argument('--debug', help="Enable debug messages", action="store_true")
+
+args=parser.parse_args()
+
+if args.pid:
+	print("Creating PID file.")
+	fh=open("/var/run/a01.pid", "w")
+	fh.write(str(os.getpid()))
+	fh.close()
+
+if args.interval:
+	print("Setting Intervalometer to take {} pictures.". format(args.interval))
+
+if args.count:
+	print("Taking a total of {} pictures." .  format(args.count))
+
+def getPage(air, link, headers):
+	getURL=air + link
+	r=requests.get(getURL, headers=headers)
+	if args.debug:
+		print(getURL)
+		print(r.text)
 
 #The Olympus Air is a DHCP server that gives itself the IP below.
 air='http://192.168.0.10/'
@@ -28,40 +57,35 @@ headers = {'user-agent': 'OlympusCameraKit', 'content-length':'4096'}
 #Try to connect to the A01, if it fails quit
 try:
 	#check mode
-	getURL=air + 'get_connectmode.cgi'
-	print(getURL)
-	r=requests.get(getURL, headers=headers)
-	print(r.text)
+	getPage(air, 'get_connectmode.cgi', headers)
 except:
 	print("ERROR: Unable to reach air at : " + air)
 	quit()
 
 #set mode
-getURL=air + 'switch_cameramode.cgi?mode=rec'
-print(getURL)
-r = requests.get(getURL, headers=headers)
-print(r.text)
+getPage(air, 'switch_cameramode.cgi?mode=rec', headers)
 
 #Get status
-getURL=air + 'get_state.cgi'
-print(getURL)
-r = requests.get(getURL, headers=headers)
-print(r.text)
+getPage(air, 'get_state.cgi', headers)
 
 #Set Live View
-getURL=air + 'exec_takemisc.cgi?com=startliveview&port=5555'
-print(getURL)
-r = requests.get(getURL, headers=headers)
-print(r.text)
+getPage(air, 'exec_takemisc.cgi?com=startliveview&port=5555', headers)
 
-#Try to take a picture
-#xml="""<?xml version='1.0' encoding='utf-8'?>
-#<>"""
-try:
-	getURL=air + 'exec_takemotion.cgi?com=newstarttake'
-	print(getURL)
-	r=requests.get(getURL, headers=headers)
-	print(r.text)
-except: 
-	print("ERROR: Unable to take photo")
+#Take one picture and exit
+if not args.count:
+	#Take a picture
+	print("Taking 1 photo.")
+	getPage(air, 'exec_takemotion.cgi?com=newstarttake', headers)
 	quit()
+
+#Take pics at specified interval
+if args.count: 
+	i=1
+	while i <= args.count:
+		print("Taking picture {} of {} ". format(i, args.count))
+		getPage(air, 'exec_takemotion.cgi?com=newstarttake', headers)
+		if args.interval and args.count != i:
+			print("Sleeping for {} seconds.". format(args.interval))
+			time.sleep(args.interval)
+		i=i+1
+
